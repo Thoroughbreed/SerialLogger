7ebd2f2fa55bfc1fb901a2cccfb7189c35613191
jan <jan@W10BOOTCAMP>
2021-12-10 11:15:41 +0100

    Init

---
+++ SerialLog/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.IO.Ports;
+
+namespace SerialLog
+{
+    class Program
+    {
+        static SerialPort inputPort = new SerialPort("COM3", 115200, Parity.None, 8, StopBits.One);
+        static string fileName = "eventlog.txt";
+        static void Main(string[] args)
+        {
+            inputPort.Open();
+            while (true)
+            {
+                string serialInput = inputPort.ReadLine();
+                if (!string.IsNullOrWhiteSpace(serialInput))
+                {
+                    Console.WriteLine(serialInput);
+                    File.AppendAllText(fileName, serialInput);
+                }
+            }
+        }
+    }
+}
