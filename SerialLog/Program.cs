3784f8f42e716ee8b115b74a0a7e36ca9685aa43
Jan Andreasen <jan@tved.it>
2021-12-12 21:45:10 +0100

    Working on simple menu and default data if no arguments is passed

--- SerialLog/Program.cs
+++ SerialLog/Program.cs
@@ -1,46 +1,107 @@
 ﻿using System;
 using System.IO;
 using System.IO.Ports;
-using System.Collections.Generic;
 
 namespace SerialLog
 {
     class Program
     {
-        static SerialPort inputPort;
-        static string fileName = "eventlog.txt";
-        static string COM = "COM3";
-        static int BAUD = 115200;
-        static int _parity = 0;
-        static Parity PARITY;
-        static int DATABITS = 8;
-        static int _stopbit = 1;
-        static StopBits STOPBIT;
+        private static ConsoleKeyInfo inputKey;
+        private static bool brk;
+        private static string fileName = "eventlog.txt";
+        private static string C = "COM3";
+        private static int B = 115200;
+        private static int _p;
+        private static int D = 8;
+        private static int _s = 1;
+        private static Parity P;
+        private static StopBits S;
+
         static void Main(string[] args)
         {
-            if (args.Length > 4)
+            if (!args.IsNullOrEmpty())
             {
-                fileName = args[0];
-                COM = args[1];
-                int.TryParse(args[2], out BAUD);
-                int.TryParse(args[3], out _parity);
-                int.TryParse(args[4], out DATABITS);
-                int.TryParse(args[5], out _stopbit);
+                if (args.Length > 0) fileName = args[0];
+                if (args.Length > 1) C = "COM" + args[1];
+                if (args.Length > 2) int.TryParse(args[2], out B);
+                if (args.Length > 3) int.TryParse(args[3], out _p);
+                if (args.Length > 4) int.TryParse(args[4], out D);
+                if (args.Length > 5) int.TryParse(args[5], out _s);
             }
-            PARITY = (Parity)_parity;
-            STOPBIT = (StopBits)_stopbit;
 
-            inputPort = new SerialPort(COM, BAUD, PARITY, DATABITS, STOPBIT);
-            inputPort.Open();
-            while (true)
+            P = (Parity)_p;
+            S = (StopBits)_s;
+
+            try
             {
-                string serialInput = inputPort.ReadLine();
-                if (!string.IsNullOrWhiteSpace(serialInput))
+                var inputPort = new SerialPort(C, B, P, D, S);
+                inputPort.Open();
+                while (true)
                 {
-                    Console.WriteLine(serialInput);
-                    File.AppendAllText(fileName, serialInput);
+                    string serialInput = inputPort.ReadLine();
+                    if (!string.IsNullOrWhiteSpace(serialInput))
+                    {
+                        Console.WriteLine(serialInput);
+                        File.AppendAllText(fileName, serialInput);
+                    }
                 }
             }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        static void Continue()
+        {
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Filename\tPort\tBAUD\tParity\tData- \tStop bit");
+                Console.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}\t{5}", fileName, C, B, P, D, S);
+                Console.WriteLine("Do you want to continue with those values?");
+                Console.WriteLine("[Y]es or [N]o");
+                inputKey = Console.ReadKey(true);
+                if (inputKey.Key == ConsoleKey.Y || inputKey.Key == ConsoleKey.N) brk = true;
+            } while (brk == false);
+        }
+
+        static void ChangeValues()
+        {
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Which attribute do you wish to change?");
+                Console.WriteLine("(Press X to leave values as is)");
+                Console.WriteLine(
+                    "[F]ilename ({0})\n[C]OM-port ({1})\n[B]AUD-rate ({2})\n[P]arity bit ({3})\n[D]ata bits ({4})\n[S]top bit ({5})"
+                    , fileName, C, B, P, D, S);
+                inputKey = Console.ReadKey(true);
+                switch (inputKey.Key)
+                {
+                    case ConsoleKey.F:
+                        Console.WriteLine("Enter new filename (leave blank to keep it)");
+                        string _input = Console.ReadLine();
+                        if (!string.IsNullOrWhiteSpace(_input))
+                        {
+                            fileName = _input;
+                        }
+                        break;
+                    case ConsoleKey.C:
+                        break;
+                    case ConsoleKey.B:
+                        break;
+                    case ConsoleKey.P:
+                        break;
+                    case ConsoleKey.D:
+                        break;
+                    case ConsoleKey.S:
+                        break;
+                    case ConsoleKey.X:
+                        brk = true;
+                        break;
+                }
+            } while (brk == false);
         }
     }
 }
