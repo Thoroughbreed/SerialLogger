85319947e0f902a30187da46d34541dd5754fd83
Jan Andreasen <jan@tved.it>
2021-12-12 20:47:35 +0100

    init

---
+++ SerialLog/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace SerialLog
+{
+    class Program
+    {
+        public SerialPort inputPort = new SerialPort("COM3", 115200, Parity.None, 8, StopBits.None);
+        public void Main(string[] args)
+        {
+            if (inputPort == null) return;
+            inputPort.ReadTimeout = 2000;
+            inputPort.Open();
+            while (true)
+            {
+                string serialInput = inputPort.ReadLine();
+                if (!string.IsNullOrWhiteSpace(serialInput))
+                {
+                    Console.WriteLine(serialInput);
+                }
+            }
+        }
+    }
+}
