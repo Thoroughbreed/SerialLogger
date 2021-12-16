2009222435d545fa832e37276b2f09774708ef39
Jan Andreasen <jan@tved.it>
2021-12-16 20:42:28 +0100

    v 0.1.1

---
+++ README.md
@@ -0,0 +1,60 @@
+![Build succeeded][build-shield]
+![Test passing][test-shield]
+[![Issues][issues-shield]][issues-url]
+[![Contributors][contributors-shield]][contributors-url]
+[![Forks][forks-shield]][forks-url]
+# SerialLogger
+
+### What is this?
+SerialLogger is a very lightweight and simple logging application, built as an extra feature of the
+[Intelligent House](https://github.com/jaa2019/H3_Embedded_Intelligent__House) project.
+
+The application is fairly straightforward and takes 5 (five) arguments in total:
+| Argument name | Formatting | Description |
+ |-|-|-|
+| | (Default value) | |
+ fileName | `eventlog.txt`| Filename of your log file. Any type is valid
+ COM | `COM3` | The serial port on your computer connected to the device
+ BAUD | `115200` | The speed of which your device is communicating
+ PARITY | `0` | 0-4 (None, Odd, Even, Mark, Space)
+ DATABIT | `8` | How many databits you're receiving
+ STOPBIT | `1` | 0-3 (None, One, Two, OnePointFive)
+
+If you launch the application without any arguments, it will fallback to default values. 
+However, if you want to launch it with arguments the following is valid:
+`SerialLog.exe eventlog.txt 3 115200 0 8 1`
+
+Before the application starts listening, you can now validate the settings (and change them if needed)
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+### Changelog
+| Version | Change |
+|-|-|
+| 0.0.5 | First release, using arguments / fallback to default |
+| 0.1.0 | Added simple menu to change values if no or incorrect arguments were given. Added control logic to limit input of databits |
+| 0.1.1 | Added a friendly exception message |
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+### License
+* Software: GPLv3
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+### Contact
+
+Jan Andreasen - [@andreasen_jan](https://twitter.com/andreasen_jan) - jan@tved.it
+
+Project Link: [https://github.com/jaa2019/SerialLogger](https://github.com/jaa2019/SerialLogger)
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+<!-- MARKDOWN LINKS & IMAGES -->
+<!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
+[build-shield]: https://img.shields.io/badge/Build-succeeded-brightgreen.svg
+[test-shield]: https://img.shields.io/badge/Tests-passing-brightgreen.svg
+[contributors-shield]: https://img.shields.io/badge/Contributors-2-brightgreen.svg
+[contributors-url]: https://github.com/jaa2019/SerialLogger/graphs/contributors
+[forks-shield]: https://img.shields.io/badge/Forks-0-blue.svg
+[forks-url]: https://github.com/jaa2019/SerialLogger/network/members
+[issues-shield]: https://img.shields.io/badge/Issues-0-brightgreen.svg
+[issues-url]: https://github.com/jaa2019/SerialLogger/issues
--- SerialLog/Program.cs
+++ SerialLog/Program.cs
@@ -1,24 +1,177 @@
 ﻿using System;
+using System.IO;
 using System.IO.Ports;
 
 namespace SerialLog
 {
     class Program
     {
-        public SerialPort inputPort = new SerialPort("COM3", 115200, Parity.None, 8, StopBits.None);
-        public void Main(string[] args)
+        private static ConsoleKeyInfo inputKey;
+        private static bool brk;
+        private static string fileName = "eventlog.txt";
+        private static string C = "COM3";
+        private static int B = 115200;
+        private static int _p;
+        private static int D = 8;
+        private static int _s = 1;
+        private static Parity P;
+        private static StopBits S;
+        private static string _input;
+
+        static void Main(string[] args)
         {
-            if (inputPort == null) return;
-            inputPort.ReadTimeout = 2000;
-            inputPort.Open();
-            while (true)
+            if (!args.IsNullOrEmpty())
             {
-                string serialInput = inputPort.ReadLine();
-                if (!string.IsNullOrWhiteSpace(serialInput))
+                if (args.Length > 0) fileName = args[0];
+                if (args.Length > 1) C = "COM" + args[1];
+                if (args.Length > 2) int.TryParse(args[2], out B);
+                if (args.Length > 3) int.TryParse(args[3], out _p);
+                if (args.Length > 4) int.TryParse(args[4], out D);
+                if (args.Length > 5) int.TryParse(args[5], out _s);
+            }
+            P = (Parity)_p;
+            S = (StopBits)_s;
+            
+            Continue();
+
+            try
+            {
+                var inputPort = new SerialPort(C, B, P, D, S);
+                inputPort.Open();
+                while (true)
                 {
-                    Console.WriteLine(serialInput);
+                    string serialInput = inputPort.ReadLine();
+                    if (!string.IsNullOrWhiteSpace(serialInput))
+                    {
+                        Console.WriteLine(serialInput);
+                        File.AppendAllText(fileName, serialInput);
+                    }
                 }
             }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        static void Continue()
+        {
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Filename\tPort\tBAUD\tParity\tData- \tStop bit");
+                Console.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}\t{5}", fileName, C, B, P, D, S);
+                Console.WriteLine("Do you want to continue with those values?");
+                Console.WriteLine("[Y]es or [N]o");
+                inputKey = Console.ReadKey(true);
+                if (inputKey.Key == ConsoleKey.Y) brk = true;
+                else if (inputKey.Key == ConsoleKey.N)
+                {
+                    ChangeValues();
+                    brk = true;
+                }
+            } while (brk == false);
+        }
+
+        static void ChangeValues()
+        {
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Which attribute do you wish to change?");
+                Console.WriteLine("(Press X to leave values as is)");
+                Console.WriteLine(
+                    "[F]ilename ({0})\n[C]OM-port ({1})\n[B]AUD-rate ({2})\n[P]arity bit ({3})\n[D]ata bits ({4})\n[S]top bit ({5})"
+                    , fileName, C, B, P, D, S);
+                inputKey = Console.ReadKey(true);
+                switch (inputKey.Key)
+                {
+                    case ConsoleKey.F:
+                        Console.WriteLine("Enter new filename (leave blank to keep it)");
+                        _input = Console.ReadLine();
+                        if (!string.IsNullOrWhiteSpace(_input))
+                        {
+                            fileName = _input;
+                        }
+                        break;
+                    case ConsoleKey.C:
+                        Console.WriteLine("Enter new COM-port (only number! - leave blank to keep it)");
+                        _input = Console.ReadLine();
+                        if (!string.IsNullOrWhiteSpace(_input))
+                        {
+                            C = "COM" + _input;
+                        }
+                        break;
+                    case ConsoleKey.B:
+                        Console.WriteLine("Enter new BAUD-rate (only number! - leave blank to keep it)");
+                        _input = Console.ReadLine();
+                        if (!string.IsNullOrWhiteSpace(_input))
+                        {
+                            int.TryParse(_input, out B);
+                        }
+                        break;
+                    case ConsoleKey.P:
+                        Console.WriteLine("Select parity bit (press enter to keep it)");
+                        Console.WriteLine("[N]one\n[O]dd\n[E]ven\n[M]ark\n[S]pace");
+                        ConsoleKeyInfo x = Console.ReadKey(true);
+                        switch (x.Key)
+                        {
+                            case ConsoleKey.N:
+                                P = (Parity)0;
+                                break;
+                            case ConsoleKey.O:
+                                P = (Parity)1;
+                                break;
+                            case ConsoleKey.E:
+                                P = (Parity)2;
+                                break;
+                            case ConsoleKey.M:
+                                P = (Parity)3;
+                                break;
+                            case ConsoleKey.S:
+                                P = (Parity)4;
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    case ConsoleKey.D:
+                        Console.WriteLine("Enter data bits (leave blank to keep it)");
+                        _input = Console.ReadLine();
+                        int y;
+                        if (!string.IsNullOrWhiteSpace(_input))
+                        {
+                            int.TryParse(_input, out y);
+                            if (y <= 8 && y >= 5) D = y;
+                        }
+                        break;
+                    case ConsoleKey.S:
+                        Console.WriteLine("Select stop bit (press enter to keep it)");
+                        Console.WriteLine("[N]one\n[O]One\n[T]wo\nOne[P]ointFive");
+                        ConsoleKeyInfo z = Console.ReadKey(true);
+                        switch (z.Key)
+                        {
+                            case ConsoleKey.N:
+                                S = (StopBits)0;
+                                break;
+                            case ConsoleKey.O:
+                                S = (StopBits)1;
+                                break;
+                            case ConsoleKey.T:
+                                S = (StopBits)2;
+                                break;
+                            case ConsoleKey.P:
+                                S = (StopBits)3;
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    case ConsoleKey.X:
+                        brk = true;
+                        break;
+                }
+            } while (brk == false);
         }
     }
 }
